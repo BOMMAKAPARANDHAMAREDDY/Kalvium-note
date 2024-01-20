68fbb171bdb7d6fa0308f6eab763bb0a0efe787b
BOMMAKAPARANDHAMAREDDY <parandhama.b@kalvium.community>
2024-01-20 13:15:13 +0530

    completed Kalvium note successfully

---
+++ pronote/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Editor from "./components/Editor";
+import "./components/Editor.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Editor />
+    </div>
+  );
+}
+
+export default App;
---
+++ pronote/src/components/Editor.jsx
@@ -0,0 +1,35 @@
+import { Component } from "react";
+
+export default class Editor extends Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      value: "",
+    };
+  }
+
+  handleChange(el) {
+    this.setState({ value: el.target.value });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="input">
+          <h3>Input</h3>
+          <textarea
+            className="input-text"
+            onChange={this.handleChange}
+            defaultValue={this.state.value}
+          />
+        </div>
+        <div className="output">
+          <h3>Pro Note</h3>
+          <div className="output-text">{this.state.value}</div>
+        </div>
+      </div>
+    );
+  }
+}
